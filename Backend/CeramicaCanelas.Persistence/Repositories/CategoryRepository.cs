c02fa6d545226580d8b291438e98f5b735708485
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-07-09 09:37:37 -0300

    feat: cadastro de categorias implementado

---
+++ Backend/CeramicaCanelas.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using CeramicaCanelas.Application.Contracts.Persistance.Repositories;
+using CeramicaCanelas.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CeramicaCanelas.Persistence.Repositories
+{
+    public class CategoryRepository(DefaultContext context) : BaseRepository<Categories>(context), ICategoryRepository
+    {
+        /// <summary>
+        /// Gets all categories from the database
+        /// </summary>
+        /// <returns>
+        /// A list of <see cref="Categories"/> entities
+        /// </returns>
+        public async Task<IEnumerable<Categories>> GetAllAsync()
+        {
+            return await Context.Categories.ToListAsync();
+        }
+        /// <summary>
+        /// Gets a category by its ID
+        /// </summary>
+        /// <param name="id">
+        /// The ID of the category to retrieve
+        /// </param>
+        /// <returns>
+        /// The <see cref="Categories"/> entity with the specified ID, or null if not found
+        /// </returns>
+        public async Task<Categories?> GetByIdAsync(Guid id)
+        {
+            return await Context.Categories.FindAsync(id);
+        }
+    }
+}
