61c4e1c05284009dffcbfc2b3155872e6d9c6ab5
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-07-09 17:18:30 -0300

    feat: implementação de cadastro de funcionários.

---
+++ Backend/CeramicaCanelas.Application/Features/Employees/Command/CreatedEmployeesCommand/CreatedEmployeesCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace CeramicaCanelas.Application.Features.Employees.Command.CreatedEmployeesCommand
+{
+    public class CreatedEmployeesCommand : IRequest<Unit>
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string CPF { get; set; } = string.Empty;
+
+        public Domain.Enums.Positiions Positiions { get; set; }
+        public IFormFile? Imagem { get; set; }
+
+
+        public Domain.Entities.Employee AssignToEmployee()
+        {
+            return new Domain.Entities.Employee
+            {
+                Name = Name,
+                CPF = CPF,
+                Positiions = Positiions,
+                CreatedOn = DateTime.UtcNow,
+                IsActive = true
+            };
+        }
+
+    }
+}
