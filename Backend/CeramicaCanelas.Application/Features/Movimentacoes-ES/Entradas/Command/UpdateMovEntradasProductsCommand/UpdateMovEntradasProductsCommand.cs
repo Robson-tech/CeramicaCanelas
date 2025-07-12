5f470fd7144a9b5181c4badea38bb027b5b15453
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-07-12 08:48:11 -0300

    feat: atualizacção de movimentações de entrada

---
+++ Backend/CeramicaCanelas.Application/Features/Movimentacoes-ES/Entradas/Command/UpdateMovEntradasProductsCommand/UpdateMovEntradasProductsCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeramicaCanelas.Application.Features.Movimentacoes_ES.Entradas.Command.UpdateMovEntradasProductsCommand
+{
+    public class UpdateMovEntradasProductsCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+        public Guid ProductEntryId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public float UnitPrice { get; set; }
+    }
+}
---
+++ Backend/CeramicaCanelas.Application/Features/Movimentacoes-ES/Entradas/Command/UpdateMovEntradasProductsCommand/UpdateMovEntradasProductsCommandHandler.cs
@@ -0,0 +1,96 @@
+﻿using CeramicaCanelas.Application.Contracts.Application.Services;
+using CeramicaCanelas.Application.Contracts.Persistance.Repositories;
+using CeramicaCanelas.Domain.Exception;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeramicaCanelas.Application.Features.Movimentacoes_ES.Entradas.Command.UpdateMovEntradasProductsCommand
+{
+    public class UpdateMovEntradasProductsCommandHandler : IRequestHandler<UpdateMovEntradasProductsCommand, Unit>
+    {
+        private readonly IMovimentacaoEntradasProductsRepository _movimentacaoESRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly ILogged _logged;
+
+        public UpdateMovEntradasProductsCommandHandler(IMovimentacaoEntradasProductsRepository movimentacaoEntradasProductsRepository,
+            IProductRepository productRepository,
+            ILogged logged)
+
+        {
+            _movimentacaoESRepository = movimentacaoEntradasProductsRepository;
+            _productRepository = productRepository;
+            _logged = logged;
+        }
+
+        public async Task<Unit> Handle(UpdateMovEntradasProductsCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _logged.UserLogged();
+
+            if (user == null)
+            {
+                throw new UnauthorizedAccessException("Usuário não autenticado.");
+            }
+
+            var movimentacaoES = await ValidateUpdateMovimentacaoES(command, cancellationToken);
+
+            // Atualiza o estoque do produto
+            var product = await _productRepository.GetProductByIdAsync(command.ProductId);
+            if (product == null)
+            {
+                throw new BadRequestException("Produto não encontrado.");
+            }
+
+            //Desatalizando a movimentação anterior
+            product.StockCurrent -= movimentacaoES.Quantity;
+            product.ValueTotal -= movimentacaoES.UnitPrice * movimentacaoES.Quantity;
+            await _productRepository.Update(product);
+
+
+            //Atualizando com os novos valores
+            movimentacaoES.ProductId = command.ProductId;
+            movimentacaoES.Quantity = command.Quantity;
+            movimentacaoES.UnitPrice = command.UnitPrice;
+            movimentacaoES.ModifiedOn = DateTime.UtcNow;
+            movimentacaoES.UserId = user.Id;
+            await _movimentacaoESRepository.Update(movimentacaoES);
+
+            // Atualiza o estoque do produto com os novos valores
+            product.StockCurrent += command.Quantity;
+            product.ValueTotal += command.UnitPrice * command.Quantity;
+            product.ModifiedOn = DateTime.UtcNow;
+            await _productRepository.Update(product);
+
+            return Unit.Value;
+        }
+
+        private async Task<Domain.Entities.ProductEntry> ValidateUpdateMovimentacaoES(UpdateMovEntradasProductsCommand command, CancellationToken cancellationToken)
+        {
+            var movimentacaoES = await _movimentacaoESRepository.GetByIdAsync(command.Id);
+            if (movimentacaoES == null)
+            {
+                throw new BadRequestException("Movimentação de entrada não encontrada.");
+            }
+
+            var product = await _productRepository.GetProductByIdAsync(command.ProductId);
+            if (product == null)
+            {
+                throw new BadRequestException("Produto não encontrado.");
+            }
+
+            var validator = new UpdateMovEntradasProductsCommandValidator();
+            var validationResult = await validator.ValidateAsync(command, cancellationToken);
+
+            if (!validationResult.IsValid)
+            {
+                throw new BadRequestException(validationResult);
+            }
+
+            return movimentacaoES;
+        }
+
+    }
+}
