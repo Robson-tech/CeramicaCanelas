ac3ce704649702637788acda293c5aff39ca48cb
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-07-14 08:03:02 -0300

    temp: criação da logia de atualização das saidas dos produtos

---
+++ Backend/CeramicaCanelas.Application/Features/Movimentacoes-ES/Saidas/Command/UpdateMovExitProdructsCommand/UpdateMovExitProdructsCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeramicaCanelas.Application.Features.Movimentacoes_ES.Saidas.Command.UpdateMovExitProdructsCommand
+{
+    internal class UpdateMovExitProdructsCommandHandler 
+    {
+    }
+}
