b04063d2d80d3583a6e5ace2edaf45281ba157a4
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-07-13 21:19:48 -0300

    feat: implementação de remoção de movimentações de entradas

---
+++ Backend/CeramicaCanelas.Application/Features/Movimentacoes-ES/Entradas/Command/DeleteMovEntradasProductsCommand/DeleteMovEntradasProductsCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using CeramicaCanelas.Application.Contracts.Application.Services;
+using CeramicaCanelas.Application.Contracts.Persistance.Repositories;
+using CeramicaCanelas.Domain.Exception;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeramicaCanelas.Application.Features.Movimentacoes_ES.Entradas.Command.DeleteMovEntradasProductsCommand
+{
+    public class DeleteMovEntradasProductsCommandHandler : IRequestHandler<DeleteMovEntradasProductsCommand, Unit>
+    {
+        private readonly IMovimentacaoEntradasProductsRepository _repository;
+        private readonly ILogged _logged;
+
+        public DeleteMovEntradasProductsCommandHandler(IMovimentacaoEntradasProductsRepository repository, ILogged logged)
+        {
+            _repository = repository;
+            _logged = logged;
+        }
+
+        public async Task<Unit> Handle(DeleteMovEntradasProductsCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _logged.UserLogged();
+
+            if (user == null)
+            {
+                throw new UnauthorizedAccessException("Usuário não autenticado.");
+            }
+
+            var movimentacaoES = await _repository.GetByIdAsync(command.Id);
+
+            if (movimentacaoES == null)
+                throw new BadRequestException("Movimentação de entrada não encontrada.");
+
+            await _repository.Delete(movimentacaoES);
+
+            return Unit.Value;
+        }
+
+    }
+
+}
+
