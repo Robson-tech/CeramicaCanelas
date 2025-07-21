8aa28c5bbd3eaeb3cd8430a6b6b6b2c82544ca69
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-07-21 09:40:33 -0300

    fix: adição de novas migrations

---
+++ Backend/CeramicaCanelas.Persistence/Migrations/20250721123920_FixTablesNUll.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CeramicaCanelas.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTablesNUll : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductEntries_AspNetUsers_UserId",
+                table: "ProductEntries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductEntries_Products_ProductId",
+                table: "ProductEntries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductExits_AspNetUsers_UserId",
+                table: "ProductExits");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductExits_Employees_EmployeeId",
+                table: "ProductExits");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductExits_Products_ProductId",
+                table: "ProductExits");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "ProductExits",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "ProductExits",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "EmployeeId",
+                table: "ProductExits",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "ProductEntries",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "ProductEntries",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductEntries_AspNetUsers_UserId",
+                table: "ProductEntries",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductEntries_Products_ProductId",
+                table: "ProductEntries",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductExits_AspNetUsers_UserId",
+                table: "ProductExits",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductExits_Employees_EmployeeId",
+                table: "ProductExits",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductExits_Products_ProductId",
+                table: "ProductExits",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductEntries_AspNetUsers_UserId",
+                table: "ProductEntries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductEntries_Products_ProductId",
+                table: "ProductEntries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductExits_AspNetUsers_UserId",
+                table: "ProductExits");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductExits_Employees_EmployeeId",
+                table: "ProductExits");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductExits_Products_ProductId",
+                table: "ProductExits");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "ProductExits",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "ProductExits",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "EmployeeId",
+                table: "ProductExits",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "ProductEntries",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "ProductEntries",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductEntries_AspNetUsers_UserId",
+                table: "ProductEntries",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductEntries_Products_ProductId",
+                table: "ProductEntries",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductExits_AspNetUsers_UserId",
+                table: "ProductExits",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductExits_Employees_EmployeeId",
+                table: "ProductExits",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductExits_Products_ProductId",
+                table: "ProductExits",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
