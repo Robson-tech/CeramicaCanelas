6cb6bec3c12e8763aec907bf26736c2ba6a6604e
Jorge Luis <jorge.luz@ufpi.edu.br>
2025-07-24 10:27:28 -0300

    fix: leve erro

--- Frontend/wwwroot/js/retiradasfuncionarios.js
+++ Frontend/wwwroot/js/retiradasfuncionarios.js
@@ -61,9 +61,7 @@ async function searchRetiradas(page = 1) {
 
     } catch (error) {
         if(typeof showErrorModal === 'function') {
-            showErrorModal({ title: "Erro na Pesquisa", detail: error.message });
         } else {
-            alert(`Erro na Pesquisa: ${error.message}`);
         }
     } finally {
         if(loadingDiv) loadingDiv.style.display = 'none';
