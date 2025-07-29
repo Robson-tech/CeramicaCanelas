0c7eaee23e925bccf993427acf4387ae3ede6029
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-07-29 09:11:54 -0300

    fix: retornando erro caso a senha nao tenha maiuscula e numeros

--- Backend/CeramicaCanelas.Application/Features/User/Commands/CreateUserCommand/CreateUserCommandValidator.cs
+++ Backend/CeramicaCanelas.Application/Features/User/Commands/CreateUserCommand/CreateUserCommandValidator.cs
@@ -1,23 +1,31 @@
 ﻿using FluentValidation;
 
-namespace CeramicaCanelas.Application.Features.User.Commands.CreateUserCommand;
-public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>{
-    public CreateUserCommandValidator() {
-        RuleFor(cpau => cpau.Email)
-            .NotNull()
-            .NotEmpty()
-            .WithMessage("O campo e-mail precisa ser preenchido");
+namespace CeramicaCanelas.Application.Features.User.Commands.CreateUserCommand
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(cpau => cpau.Email)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("O campo e-mail precisa ser preenchido");
 
-        RuleFor(cpau => cpau.Email)
-            .EmailAddress()
-            .WithMessage("O campo e-mail precisa ser um e-mail válido");
+            RuleFor(cpau => cpau.Email)
+                .EmailAddress()
+                .WithMessage("O campo e-mail precisa ser um e-mail válido");
 
-        RuleFor(cpau => cpau.Password)
-            .MinimumLength(6)
-            .WithMessage("O campo senha precisa ter, pelo menos, 6 caracteres");
+            RuleFor(cpau => cpau.Password)
+                .NotEmpty()
+                .MinimumLength(6).WithMessage("O campo senha precisa ter, pelo menos, 6 caracteres")
+                .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$")
+                .WithMessage("A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula e um número.")
+                .Matches(@"[!@#$%^&*(),.?""{}|<>]")
+                .WithMessage("A senha deve conter pelo menos um caractere especial.");
 
-        RuleFor(cpau => cpau.PasswordConfirmation)
-            .Matches(cpau => cpau.Password)
-            .WithMessage("O campo de confirmação de senha deve ser igual ao campo senha");
+            RuleFor(cpau => cpau.PasswordConfirmation)
+                .Equal(cpau => cpau.Password)
+                .WithMessage("O campo de confirmação de senha deve ser igual ao campo senha");
+        }
     }
 }
