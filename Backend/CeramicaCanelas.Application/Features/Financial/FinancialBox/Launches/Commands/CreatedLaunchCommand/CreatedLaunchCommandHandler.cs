47324e103712bd37fe9fa81fd55c2ddaa958e697
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-07-30 17:02:05 -0300

    fix: criação de arquivos do sistema de lançamento

---
+++ Backend/CeramicaCanelas.Application/Features/Financial/FinancialBox/Launches/Commands/CreatedLaunchCommand/CreatedLaunchCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeramicaCanelas.Application.Features.Financial.FinancialBox.Launches.Commands.CreatedLaunchCommand
+{
+    public class CreatedLaunchCommandHandler
+    {
+    }
+}
