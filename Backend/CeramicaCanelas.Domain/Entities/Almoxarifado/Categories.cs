84cf7f65b9ca0c33f1cbd9e03899b84af005203e
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-08-08 14:21:05 -0300

    fix: subindo correção de migration

--- Backend/CeramicaCanelas.Domain/Entities/Almoxarifado/Categories.cs
+++ Backend/CeramicaCanelas.Domain/Entities/Almoxarifado/Categories.cs
@@ -7,7 +7,7 @@ namespace CeramicaCanelas.Domain.Entities.Almoxarifado
     public class Categories : BaseEntity
     {
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
         public virtual ICollection<Products> Products { get; set; } = new List<Products>();
 
     }
